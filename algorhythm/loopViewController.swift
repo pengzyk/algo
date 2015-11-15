deaf155fc7c7d73229f87db74372f5457667f114
cheng <gnuchex@gmail.com>
2015-11-15 00:59:15 -0800

    click button to play single sound

--- algorhythm/loopViewController.swift
+++ algorhythm/loopViewController.swift
@@ -7,31 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
-class loopViewController: UIViewController {
 
+class loopViewController: UIViewController , AVAudioPlayerDelegate {
+    
+    
+    var audioPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("view did load")
 
-        // Do any additional setup after loading the view.
-    }
+        let mp3Path = NSBundle.mainBundle().pathForResource("Pong", ofType: "wav")
 
+        let fileURL = NSURL.fileURLWithPath(mp3Path!)
+        
+        audioPlayer = try! AVAudioPlayer(contentsOfURL: fileURL)
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+        
+
+    
+    }
+    
+    @IBAction func onClickPongButton(sender: UIButton) {
+        
+        audioPlayer.play()
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
