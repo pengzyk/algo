90788258b2c62be87805d2ebbc461425a7ec0e68
cheng <gnuchex@gmail.com>
2015-11-20 00:36:32 -0800

    slightly cleaned up code

--- algorhythm/loopViewController.swift
+++ algorhythm/loopViewController.swift
@@ -13,37 +13,29 @@ import AVFoundation
 var bonkArray = [AVAudioPlayer]()
 
 
-
-
 class loopViewController: UIViewController , AVAudioPlayerDelegate {
     
     
     var audioPlayer : AVAudioPlayer!
     var newPlayer : AVAudioPlayer! //has to be declared here..
-    var timeSlots = [AVAudioPlayer?](count:16, repeatedValue: nil)
-    
+    var ticSlots = [AVAudioPlayer?](count:16, repeatedValue: nil)
     var ticCounter = 0
-    let INTERVAL = 75.0/60.0/16.0
+    
+    let INTERVAL = 75.0/60.0/16.0     //75  per min
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("timeSlots", timeSlots.count)
- print("in viewDidiLoad timeSlots", self.timeSlots)
         let mp3Path = NSBundle.mainBundle().pathForResource("Pong", ofType: "wav")
-
         let fileURL = NSURL.fileURLWithPath(mp3Path!)
-        
         audioPlayer = try! AVAudioPlayer(contentsOfURL: fileURL)
         audioPlayer.delegate = self
         audioPlayer.prepareToPlay()
 
         
-        //add one player to the array 
-        
+        //add one player to the array
         bonkArray.append(audioPlayer)
         
-//        tick()
     
     }
     
@@ -66,49 +58,35 @@ class loopViewController: UIViewController , AVAudioPlayerDelegate {
         
     }
     
-    func prepareAVAudioPlayer(fileName: String, fileType: String) -> AVAudioPlayer {
-        let path = NSBundle.mainBundle().pathForResource(fileName, ofType: fileType)
-        let fileURL = NSURL.fileURLWithPath(path!)
-        let tempPlayer = try! AVAudioPlayer(contentsOfURL: fileURL)
-        tempPlayer.delegate = self
-        tempPlayer.prepareToPlay()
-        return tempPlayer
-    }
-    
-  
     @IBAction func onClickSquare(sender: AnyObject) {
-        //add drum beats at 4,8,12,16 positions
-        timeSlots[0] = prepareAVAudioPlayer( "WoodBonk", fileType: "wav")
-                timeSlots[4] = prepareAVAudioPlayer( "WoodBonk", fileType: "wav")
-                timeSlots[8] = prepareAVAudioPlayer( "WoodBonk", fileType: "wav")
-                timeSlots[12] = prepareAVAudioPlayer( "WoodBonk", fileType: "wav")
-        
-        //print("timeSlots", timeSlots.count)
+        //add drum beats at 4,8,12,16 positions (a square shape!)
+        ticSlots[0] = prepareAVAudioPlayer( "WoodBonk", fileType: "wav")
+        ticSlots[4] = prepareAVAudioPlayer( "WoodBonk", fileType: "wav")
+        ticSlots[8] = prepareAVAudioPlayer( "WoodBonk", fileType: "wav")
+        ticSlots[12] = prepareAVAudioPlayer( "WoodBonk", fileType: "wav")
         
     }
     
- 
-    
-    //75  per min
 
     @IBAction func onClickPlayButton(sender: AnyObject) {
-        print("play")
-        //play in sequence
-        print("in play timeSlots", self.timeSlots.count)// 16
+//        print("play")
+//        print("in play timeSlots", self.timeSlots.count)// 16
         
-        NSTimer.scheduledTimerWithTimeInterval(INTERVAL, target: self, selector: "tick", userInfo: timeSlots as? AnyObject, repeats: true)
+        //start a timer that loops through the array
+        NSTimer.scheduledTimerWithTimeInterval(INTERVAL, target: self, selector: "tick", userInfo: ticSlots as? AnyObject, repeats: true)
     }
 
+    //check each of the slot in ticSlots and play the audio, if occupied
     func tick(){
-
 //        print("[tick] timeSlots size \(self.timeSlots.count). tickCounter \(self.ticCounter)")
-        if ( self.timeSlots[self.ticCounter] != nil) {
+        if ( self.ticSlots[self.ticCounter] != nil) {
             print("playing at slot ", self.ticCounter) ;
-            self.timeSlots[self.ticCounter]?.play()
-//
+            self.ticSlots[self.ticCounter]?.play()
         }
+        
+        //ticCounter loops through 0-15
         self.ticCounter++
-        if(self.ticCounter >= self.timeSlots.count){
+        if(self.ticCounter >= self.ticSlots.count){
             self.ticCounter = 0
         }
         
@@ -117,7 +95,6 @@ class loopViewController: UIViewController , AVAudioPlayerDelegate {
     
     @IBAction func onClickBoingButton(sender: AnyObject) {
         //        var newPlayer : AVAudioPlayer! //this variable is created when the function starts and is killed when the function ends
-        
         let newPath = NSBundle.mainBundle()
             .pathForResource("String", ofType: "wav")
         let newFileURL = NSURL.fileURLWithPath(newPath!)
@@ -142,7 +119,20 @@ class loopViewController: UIViewController , AVAudioPlayerDelegate {
     @IBAction func onClickXButton(sender: AnyObject) {
         bonkArray.removeAll()
         
+        //todo: return ticSlots to array of 16 nils
+        
     }
+    
+    //load the audio files given the file names
+    func prepareAVAudioPlayer(fileName: String, fileType: String) -> AVAudioPlayer {
+        let path = NSBundle.mainBundle().pathForResource(fileName, ofType: fileType)
+        let fileURL = NSURL.fileURLWithPath(path!)
+        let tempPlayer = try! AVAudioPlayer(contentsOfURL: fileURL)
+        tempPlayer.delegate = self
+        tempPlayer.prepareToPlay()
+        return tempPlayer
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
