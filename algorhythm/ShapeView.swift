f66ce27298868cf75133bb13aa30328659f4d277
Timothy Lee <tim@codepath.com>
2015-12-16 17:38:22 -0800

    Add ShapeView implementation

--- algorhythm/ShapeView.swift
+++ algorhythm/ShapeView.swift
@@ -9,13 +9,65 @@
 import UIKit
 
 class ShapeView: UIView {
+    
+    var soundFile: String!
+    var numPoints: Int!
+    
+    var anchorView: UIView!
+    
+    func setup() {
+        soundFile = "just blaze bksnare2"
+        numPoints = 3
+        
+        anchorView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        anchorView.userInteractionEnabled = true
+        anchorView.backgroundColor = UIColor.purpleColor()
+        anchorView.center = CGPoint(x: 0, y: 0)
+        addSubview(anchorView)
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: "onLongPress:")
+        gestureRecognizer.minimumPressDuration = 0
+        anchorView.addGestureRecognizer(gestureRecognizer)
+        
+        backgroundColor = UIColor.orangeColor()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
 
-    /*
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        setup()
+    }
+
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
         // Drawing code
+        
+        let path = UIBezierPath()
+        path.moveToPoint(anchorView.center)
+        path.addLineToPoint(CGPoint(x: 50, y: 50))
+        path.addLineToPoint(CGPoint(x: 0, y: 50))
+        
+        UIColor.blueColor().setFill()
+        
+        path.closePath()
+        path.fill()
     }
-    */
+    
+    func onLongPress(sender: UILongPressGestureRecognizer) {
+        let v = sender.view!
+        let location = sender.locationInView(self)
+        
+        v.center = location
+        
+        // Triggers drawRect
+        setNeedsDisplay()
+    }
 
 }
